01d0901828374b2e163df46616b4ae8ec97d40ec
sjordan1122 <111324697+sjordan1122@users.noreply.github.com>
2022-08-17 10:01:50 +0800

    Add files via upload

---
+++ themes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Custom Themes
+|--------------------------------------------------------------------------
+|
+| List of custom themes and it's repository link availbable for cloning 
+| for the fresh installation of the WordPress
+|
+*/
+
+return [
+    
+    
+    "hello-elementor" => [
+        "label" => "Hello Elementor",
+        "name" => "hello-elementor",
+        "repo" => "https://github.com/devmode2022/hello-elementor.git"
+    ],
+
+    "oceanwp" => [
+        "label" => "Ocean WP",
+        "name" => "oceanwp",
+        "repo" => "https://github.com/devmode2022/oceanwp.git"
+    ],
+
+    "popularfx" => [
+        "label" => "Popular FX",
+        "name" => "popularfx",
+        "repo" => "https://github.com/devmode2022/popularfx.git"
+    ]
+];
